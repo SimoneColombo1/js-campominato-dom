476cb0238f08db458f0d24bd3f8b140501cb00f9
SimoneColombo1 <simcolombo2@gmail.com>
2024-05-20 12:05:29 +0200

    creato lo scafolding  e copiato il file html css e js

---
+++ JS/script.js
@@ -0,0 +1,66 @@
+
+//* dichiarazione variabili//
+const griglia= document.querySelector('main #griglia');
+const StartButton= document.querySelector('header #start');
+
+//!evento// 
+
+
+StartButton.addEventListener('click',function(){
+
+//todo ciclo for//
+griglia.innerHTML="";
+
+for( let i=0 ; i<100;i++ ){
+
+
+
+    //? creazione quadrati//
+const quadrati= document.createElement('div');
+quadrati.classList.add('square');
+
+
+
+//$ creazione span con i numeri//
+ 
+const elementi= document.createElement('span');
+elementi.append(i+1);
+quadrati.appendChild(elementi);
+
+
+
+//^ Colorare quadrati al click//
+
+quadrati.addEventListener('click', function(){
+ 
+    quadrati.classList.add('active');
+
+    console.log(i+1);
+
+
+})
+
+griglia.appendChild(quadrati);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+)
